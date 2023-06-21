ccfa46c37f4daf1a6650284c75ccf6c20bb5bb98
Umoh Samuel <samuelwisdpom360@gmail.com>
2023-06-21 22:05:37 +0100

    add logging functionality to write to both a file and standard output

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,30 @@
 import pino from "pino";
 
-const logger = pino(
-  {
-    transport: {
+const transport = pino.transport({
+  targets: [
+    {
+      target: 'pino/file',
+      options: { destination: `./logs/app.log` },
+      level: 'info'
+    },
+    {
       target: 'pino-pretty',
       options: {
         colorize: true,
         translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
         ignore: 'pid,hostname'
-      }
-    },
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+      },
+      level: 'info'
+    }
+  ]
+});
+
+const logger = pino(
+  {
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    timestamp: pino.stdTimeFunctions.isoTime
   },
-  pino.destination(`${__dirname}/app.log`)
+  transport
 );
 
 export default logger;
