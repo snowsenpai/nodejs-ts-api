9ff53690f70365c7bb0040524eede14492502136
Umoh Samuel <samuelwisdpom360@gmail.com>
2023-10-03 17:55:32 +0100

    reduce duplicated code in authenticated.middleware.spec

--- src/middleware/authenticated.middleware.spec.ts
+++ src/middleware/authenticated.middleware.spec.ts
@@ -5,19 +5,38 @@ import { HttpStatus, HttpException } from '@/utils/exceptions';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { sampleUser } from 'tests/sample-data';
 import { JsonWebTokenError } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
 
 const authError = new HttpException(HttpStatus.UNAUTHORIZED, 'You are not authorized');
 
-// TODO refactor tests, dry, try using spyRefrence: jest.Mock and beforeEach
-
 describe('Authentication middleware', () => {
+  let req: Request;
+  let res: Response;
+  let next: NextFunction;
+  let verifyTokenSpy: jest.SpyInstance;
+  let findByIdSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    req = getMockReq();
+    ({ res, next } = getMockRes());
+
+    verifyTokenSpy = jest
+      .spyOn(token, 'verifyToken')
+      // @ts-ignore
+      .mockImplementation((accessToken) => {
+        if (accessToken.includes('accessToken')) return Promise.resolve({ id: sampleUser._id });
+        return Promise.reject(new JsonWebTokenError('invalid token'));
+      });
+
+    findByIdSpy = jest
+      .spyOn(UserModel, 'findById')
+      // @ts-ignore
+      .mockReturnValue({
+        exec: jest.fn().mockResolvedValue(sampleUser),
+      });
+  });
+
   it('should call next with an HttpException if authorization header is undefined', async () => {
-    const req = getMockReq();
-    const { res, next } = getMockRes();
-
-    const verifyTokenSpy = jest.spyOn(token, 'verifyToken');
-    const findByIdSpy = jest.spyOn(UserModel, 'findById');
-
     await authenticatedMiddleware(req, res, next);
 
     expect(verifyTokenSpy).not.toHaveBeenCalled();
@@ -27,14 +46,7 @@ describe('Authentication middleware', () => {
   });
 
   it('should call next with an HttpException if authorization header does not start with `Bearer `', async () => {
-    const req = getMockReq({
-      headers: {
-        authorization: 'bearer sustoken',
-      },
-    });
-    const { res, next } = getMockRes();
-    const verifyTokenSpy = jest.spyOn(token, 'verifyToken');
-    const findByIdSpy = jest.spyOn(UserModel, 'findById');
+    req.headers.authorization = 'bearer sustoken';
 
     await authenticatedMiddleware(req, res, next);
 
@@ -45,20 +57,7 @@ describe('Authentication middleware', () => {
   });
 
   it('should call next with an HttpException if authorization header token is invalid', async () => {
-    const req = getMockReq({
-      headers: {
-        authorization: 'Bearer sustoken',
-      },
-    });
-    const { res, next } = getMockRes();
-    const verifyTokenSpy = jest
-      .spyOn(token, 'verifyToken')
-      // @ts-ignore
-      .mockImplementation((accessToken) => {
-        if (accessToken.includes('accessToken')) return Promise.resolve({ id: sampleUser._id });
-        return Promise.reject(new JsonWebTokenError('invalid token'));
-      });
-    const findByIdSpy = jest.spyOn(UserModel, 'findById');
+    req.headers.authorization = 'Bearer sustoken';
 
     await authenticatedMiddleware(req, res, next);
 
@@ -69,58 +68,23 @@ describe('Authentication middleware', () => {
   });
 
   it('should call next if a user is not found', async () => {
-    const req = getMockReq({
-      headers: {
-        authorization: 'Bearer accessToken',
-      },
-    });
-    const { res, next } = getMockRes();
-    const verifyTokenSpy = jest
-      .spyOn(token, 'verifyToken')
-      // @ts-ignore
-      .mockImplementation((accessToken) => {
-        if (accessToken.includes('accessToken')) return Promise.resolve({ id: sampleUser._id });
-        return Promise.reject(new JsonWebTokenError('invalid token'));
-      });
+    req.headers.authorization = 'Bearer accessToken';
 
     // e.g a token with a deleted user id
-    const findByIdSpy = jest
-      .spyOn(UserModel, 'findById')
-      // @ts-ignore
-      .mockReturnValue({
-        exec: jest.fn().mockResolvedValue(null),
-      });
-    const notFoundError = new HttpException(HttpStatus.NOT_FOUND, 'User not found');
+    findByIdSpy.mockReturnValue({
+      exec: jest.fn().mockResolvedValue(null),
+    });
 
     await authenticatedMiddleware(req, res, next);
 
     expect(verifyTokenSpy).toHaveBeenCalledWith('accessToken');
     expect(findByIdSpy).toHaveBeenCalledWith(sampleUser._id);
     expect(req.user).toBeUndefined();
-    expect(next).toHaveBeenCalledWith(notFoundError);
+    expect(next).toHaveBeenCalledWith(new HttpException(HttpStatus.NOT_FOUND, 'User not found'));
   });
 
   it('should set the user in the request object and call next if token is valid', async () => {
-    const req = getMockReq({
-      headers: {
-        authorization: 'Bearer accessToken',
-      },
-    });
-    const { res, next } = getMockRes();
-
-    const verifyTokenSpy = jest
-      .spyOn(token, 'verifyToken')
-      // @ts-ignore
-      .mockImplementation((accessToken) => {
-        if (accessToken.includes('accessToken')) return Promise.resolve({ id: sampleUser._id });
-        return Promise.reject(new JsonWebTokenError('invalid token'));
-      });
-    const findByIdSpy = jest
-      .spyOn(UserModel, 'findById')
-      // @ts-ignore
-      .mockReturnValue({
-        exec: jest.fn().mockResolvedValue(sampleUser),
-      });
+    req.headers.authorization = 'Bearer accessToken';
 
     await authenticatedMiddleware(req, res, next);
 
